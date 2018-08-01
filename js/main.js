3c09dbc6abea8afe0aa95b4455ba334be99a5efd
Oriva <weelman93@gmail.com>
2018-08-01 16:31:33 +0800

    save

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     var tel = $('#tel');
     $(document).on('scroll', function () {
+        if ($('.footer').offset().top - $(window).height()<$(window).scrollTop()) {
+            $('.tel').css('bottom', $(window).scrollTop() + $(window).height() - $('.footer').offset().top);
+        } else {
+            $('.tel').css('bottom', '0');
+        }
         var position = $(window).scrollTop();
         var block_position = $('#box1').offset().top;
         if (position > block_position) {
@@ -24,11 +29,45 @@ $('.get__item').click(function(){
         $('.get__text').removeClass('scale-text');
     }, 170);
 });
-$(".header a").click(function (event) {
+$('.header a').click(function (event) {
     if($(this).attr('href').substring(0,1)=='#'){
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     }
+});
+$('.get-form').click(function() {
+    $('.modal-enter').show();
+});
+$('.modal-enter .closed').click(function(){
+    $('.modal-enter').hide();
+});
+
+
+//forms
+
+$('.sent__form').on('submit', function(){
+    e.preventDefault();
+    var data_form = $(this).serialize();
+    console.log(data_form);
+    var phone = $(this).find('.input--tel');
+    if(phone.val().length!=16){
+        alert('Слишком короткий номер');
+    } else {
+        var needname = $('.saleform').find('.name_hid').val();
+        yaCounter49117123.reachGoal('send_sale');
+        $.ajax({
+            type: "POST",
+            url: "/sale_send.php",
+            data: data_form,
+            success: function() {
+                
+            },
+            error: function() {
+                
+            }
+        });
+    }
+    return false;
 });
